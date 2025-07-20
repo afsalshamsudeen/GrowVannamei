dc3813bb177d669c0e816c564c464231e778ddfd
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2025-07-20 20:05:33 +0530

    File upload front end logic complete

--- Client/vite-project/src/components/Card.jsx
+++ Client/vite-project/src/components/Card.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from './Title'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
+import Upload from './Upload';
 
 const Card = () => {
+
+  const [open, setOpen] = useState(false);
+  const [csvData, setCsvData] = useState(null);
+  const [csvFileName, setCsvFileName] = useState('');
+
+  const handlePredict = ()=>{
+    if(!csvData){
+      alert("Please upload a csv file first");
+      return;
+    }
+    console.log('Sending data to back end: ',csvData);
+
+    //TODO Axios logic to send csv file to backend
+  }
+
   return (
+    <>
+    
     <div className='flex items-center flex-col gap-4 pt-40 mt-10 w-140 h-170 bg-[#0b1423] rounded-md '>
       <h1 className='font-inter text-center text-4xl text-white font-bold'>Shrimp Growth <br/>& <br/>Feed Predictor</h1>
       <p className='text-white'>Upload your shrimp data CSV and get growth predictions</p>
 
-      <div className='flex items-center justify-center flex-col text-white w-96 h-20 bg-[#101a2a] rounded-md cursor-pointer' >
+      <div className='flex items-center justify-center flex-col text-white w-96 h-20 bg-[#101a2a] rounded-md cursor-pointer' onClick={()=> setOpen(true)}>
         <DriveFolderUploadIcon/>
-        <h2 className='font-medium'>Upload Shrimp Data (.csv)</h2>
+        
+        <h2 className='font-medium'> {csvFileName ? `selected File: ${csvFileName}` : 'Upload Shrimp Data (.csv)'} </h2>
+        
         </div>
-      <button className='bg-blue-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer hover:bg-blue-600'> Upload and Predict</button>
+      <button onClick={handlePredict} className='bg-blue-500 text-white px-4 py-2 rounded-md font-medium cursor-pointer hover:bg-blue-600'> Upload and Predict</button>
     </div>
+     {open && (
+      
+      <div className='fixed inset-0 flex items-center justify-center bg-black/30 z-50'>
+      <Upload setOpen={setOpen} setCsvData={setCsvData} setCsvFileName={setCsvFileName}/>
+      </div>
+      
+      )}
+    </>
   )
 }
 
--- Client/vite-project/src/components/Upload.jsx
+++ Client/vite-project/src/components/Upload.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-const Upload = () => {
+
+const Upload = ({ setOpen, setCsvData, setCsvFileName }) => {
+
+  const fileInputRef = useRef(null);
+  const [fileName, setFileName] = useState('');
+
+  const handleFileSelect = (e) =>{
+    const file = e.target.files[0];
+    if (file && file.type ==='text/csv'){
+      setFileName(file.name);
+      setCsvFileName(file.name);
+
+      const reader = new FileReader();
+      reader.onload = (event) =>{
+        const csvContent = event.target.result;
+        setCsvData(csvContent)
+        console.log('csv content:', csvContent);
+      };
+      reader.readAsText(file);
+    }else{
+      alert('Please upload a valid CSV file!')
+    }
+  };
+  const handleBrowseClick =()=>{
+    fileInputRef.current.click();
+  }
   return (
     <div className='text-white h-150 w-300 bg-[#0b1423] rounded-md flex items-center flex-col gap-10 border border-white-300' >
       <h1 className='mt-10' >SELECT DATASET</h1>
-      <div className='flex items-center justify-center flex-col mt-30 text-white w-190 h-60 bg-[#101a2a] rounded-md cursor-pointer border border-white-300'>
+      
+      <div className='flex items-center justify-center flex-col mt-30 text-white w-190 h-60 bg-[#2d3036] rounded-md cursor-pointer border border-white-300'
+          onClick={handleBrowseClick}
+        >
         <CloudUploadIcon/>
-        <p>Browse or drop image</p>
+        <p>{fileName ? fileName: 'Browse or drop image'}</p>
+        <input
+          type='file'
+          accept='.csv'
+          ref={fileInputRef}
+          onChange={handleFileSelect}
+          style={{display: 'none'}}
+        />
       </div>
+
       <p>We think data protection is important! No data is sent. The magic happens in your browser.</p>
-      <button className='w-300 cursor-pointer border border-white-300'>Cancel</button>
+      <button onClick={()=>setOpen(false)} className='w-300 cursor-pointer border border-white-300'>Back</button>
     </div>
   )
 }
